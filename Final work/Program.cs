12036a9ad9943f1ddc349592a4a349ca6fe0f5a8
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-28 23:01:26 +0300

    Сделала програамму по формированию массива строк

---
+++ Final work/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] array = new string[5] { "Hello World", ";)", "Ice", "Hello Bali", "yes" };
+string[] array3 = new string[array.Length];
+
+PrintArray(array, "Исходный массив:");
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        array3[count++] = array[i];
+}
+
+PrintArray(array3, "Массив из строк 3 символа или менее:");
+
+
+static void PrintArray(string[] array, string title)
+{
+    Console.WriteLine(title);
+    foreach (var s in array)
+        Console.WriteLine(s);
+    Console.WriteLine();
+}
